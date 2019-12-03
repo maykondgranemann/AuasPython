48e4d2c5eba32a3bb4f35d564e75a1d4e69966e0
abioluz <abioluz@gmail.com>
2019-12-03 07:46:59 -0300

    Novos exercicios

--- 17-Aula17/exercicio/exercicio2.py
+++ 17-Aula17/exercicio/exercicio2.py
@@ -1,5 +1,45 @@
-# 1 - Faça um menu interativo que tenha: Cadastro da banda, cadastro do album, cadastro da musica, Sair.
+# 1 - Faça um menu interativo que tenha: Cadastro da banda, cadastro 
+# do album, cadastro da musica, Sair.
 # O menu deve ser executado até que se escolha a opção sair.
 # Cada opção deve ser mostrada 
-# quando selecionado a opção sair, deverá aparecer na tela as informações das banda cadastradas, 
+# quando selecionado a opção sair, deverá aparecer na tela as 
+# informações das banda cadastradas, 
 # albuns e musicas.
+
+lista_banda = []
+lista_album = []
+lista_musica = []
+
+menu = '''
+#############################################################################
+                             Cadastro Faixas
+#############################################################################
+
+1 - Cadastro banda
+2 - Cadastro album
+3 - Cadastro musica
+4 - sair
+
+Digite a opção: '''
+
+while True:
+    opcao = input(menu)
+    if opcao == '1':
+        lista_banda.append(input('Digite o nome da banda: '))
+    elif opcao == '2':
+        lista_album.append(input('Digite o nome da album: '))
+    elif opcao == '3':
+        lista_musica.append(input('Digite o nome da musica: '))
+    elif opcao == '4':
+        print(f'lista de banda: {lista_banda}\nlista de album: {lista_album}\nlista de musica: {lista_musica}')
+        break
+    else:
+        print('opção invalida')
+
+
+
+    
+
+
+
+
---
+++ 18-Aula18/exercicios/exercicio4.py
@@ -0,0 +1,20 @@
+# Aula 18 - 03-11-2019
+
+# interação de lista com o for
+
+# Usando o comando for vamos fazer uma interação de varialvel tipo coleção. A interação é (simplificadamente) 
+# percorer toda a variavel e isolar seu valores.
+
+# 1.1 Com a seguinte lista, use o for para interar esta tupla  e apresentar (usando o f-string) O nome da cerveja, 
+# tipo da cerveja, o IBU da cerveja e o preço dela.
+
+cerveja = (('marca', 'tipo', 'ibu','preço'),
+           ('Skol','IPA','ultra-leve',3.50),
+           ('Brahma','lager','leve/media',3.45),
+           ('Kaiser','Americam Larger','leve',2.35),
+           ('Sol','larger mão','agua',1.19)
+          )
+
+
+# 1.2 Crie uma função que receba esta tupla e devolva uma lista com um dicionários referenciando cada uma destas 
+# cervejas.
