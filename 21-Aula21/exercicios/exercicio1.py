df785f278f1f3d0344e8ce37a549375fa7266713
abioluz <abioluz@gmail.com>
2019-12-05 22:51:34 -0300

    Aula21

---
+++ 21-Aula21/exercicios/exercicio1.py
@@ -0,0 +1,15 @@
+# Aula 21 - 06-12-2019
+# Como Tratar e Trabalhar Erros!!!
+
+# 1 - Crie um arquivo que leia 2 números inteiros e imprima a soma, 
+# multiplicação, divisão e subtração com uma f-string.
+
+# 2 - Crie um while que pergunte se deseja continuar. Se digitar 's' o
+# programa termina.
+
+#################### até aqui tudo bem! ##########################
+
+# 3 - faça um tratamento de exceção para que ao digitar o valor que 
+# não seja inteiro, ele avise o usuário para ele digitar denovo.
+# 4 - Faça outro tratamento de exceção para evitar que divida um numero
+# por zero.
