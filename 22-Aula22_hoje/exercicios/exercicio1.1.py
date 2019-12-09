aae56a5b881ea6afc8f576d841da50bf7d99c996
abioluz <abioluz@gmail.com>
2019-12-09 09:25:32 -0300

    Exercicio 1.1 upando

--- 22-Aula22_hoje/exercicios/exercicio1.1.py
+++ 22-Aula22_hoje/exercicios/exercicio1.1.py
@@ -1,6 +1,20 @@
 # Aula 21 - 09-12-2019
 
-Crie uma classe cliente
+# Crie uma classe cliente:
 
-deve ter como atributos 
+# 1) deve ter como atributos: codigo, cpf, nome, idade, sexo
+
+# 2) como metodos: receber salario, comprar, pagar divida
+
+# Quando recebe aumenta o dinheiro na carteira.
+
+# quando compra aumenta os bens e diminui o dinheiro na carteira
+
+# Se comprar e não tiver dinheiro o suficiente deve diminuir o dinheiro da carteira
+# e aumentar a divida.
+
+# Para pagar a divida tem que ter dinheiro o suficiente na carteira
+
+# 3) atributos de estado: dinheiro na carteira, divida, bens
+
 
