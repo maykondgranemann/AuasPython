b6f7483706230f7b09c67552a3fec722e781d613
Maykon D. Granemann <maykondgranemann@gmail.com>
2020-01-20 11:02:53 -0300

    css 2

--- 36-Aula36/View/web.py
+++ 36-Aula36/View/web.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import sys
 sys.path.append('/Users/mdgranemann/Documents/Github/AlunosPython/TrabalhosPython/36-Aula36')
 from Controller.pessoa_controller import PessoaController
+from Controller.endereco_controller import EnderecoController
 
 app = Flask(__name__)
 pessoa_controller = PessoaController()
+end_controller = EnderecoController()
 nome = 'Cadastros'
 
 @app.route('/')
@@ -20,4 +22,17 @@ def listar():
 def cadastrar():
     return render_template('cadastrar.html', titulo_app = nome)
 
+@app.route('/editar')
+def editar():
+    id = request.args['id']
+    return f'O id selecionado foi {id}'
+
+@app.route('/excluir')
+def excluir():
+    id = int(request.args['id'])
+    id_end = request.args['id_end']
+    pessoa_controller.deletar(id)
+    if id_end != 'None':
+        end_controller.deletar(id_end)
+    return redirect('/listar')
 app.run(debug=True)
