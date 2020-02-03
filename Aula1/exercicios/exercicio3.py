1a037e01ccd0d69c58fae5e5df010a577c0bc46f
abioluz <abioluz@gmail.com>
2020-02-02 23:20:08 -0300

    atualizando nome 2

--- Aula1/exercicios/exercicio3.py
+++
@@ -1,3 +0,0 @@
-#--- Exercicio 3  - Impressão de dados com a função Print
-#--- Imprima a mensagem - "Padawans Python HBSIS" no console
-#--- A frase deve ser escrita com asterisco em formato grande para ser usada em um cabeçalho de um programa console
--- Aula3/exercicio/exercicio1.py
+++
@@ -1,5 +0,0 @@
-#--- Exercicio 1  - Input, Estrutura de decisão e operações matemáticas
-#--- Crie um programa que leia dois números inteiros
-#--- Realize as 4 operações matemáticas básicas com os números lidos
-#--- Imprima os resultados das operações 
-#--- Informe qual número é maior ou se os dois são iguais
